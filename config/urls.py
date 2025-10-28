80557fe83495d6580bbaa0b6b13850028da3bc5a
kfbk <satou1497@gmail.com>
2025-10-28 09:15:32 +0900

    google登録8

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,12 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from .sitemaps import StaticViewSitemap
+
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
